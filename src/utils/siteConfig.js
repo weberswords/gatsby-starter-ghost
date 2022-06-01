b5addcb2c100a4a48f40a0244d7e198e0c32d46e
Webs <weberswords@gmail.com>
2022-06-01 12:39:52 -0700

    Update site url

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
             ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
-            : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+            : `https://websonthewebs.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
